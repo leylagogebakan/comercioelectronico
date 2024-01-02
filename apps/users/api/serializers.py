bfc475f5c6e8e8d355a7d27d9dfae3e928375194
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2024-01-02 22:10:41 +0100

    enciptado de password en update

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -9,12 +9,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
-    # Encrriptado de contrase;a
+    # Encrriptado de password en create
     def create(self, validated_data):
         user = User(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+    # Encriptado de passowrd en update
+    def update(self, instance, validated_data):
+        update_user = super().update(instance, validated_data)
+        update_user.set_password(validated_data['password'])    
+        update_user.save()
+        return update_user
+
 
 class UserListSerializer(serializers.ModelSerializer):
     """ Serializer for User. Method GET """
