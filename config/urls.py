3767e754a710265dded6aa348b49497f5b60fb75
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2024-01-03 21:47:58 +0100

    Serialziadores y Views generios

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-   path('users/',include('apps.users.api.urls')) 
+    path('users/',include('apps.users.api.urls')), 
+    path('products/',include('apps.products.api.urls')) 
 ]
 
