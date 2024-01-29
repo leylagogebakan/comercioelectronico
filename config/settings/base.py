3329674d5cf619a4fea42be029dafe5295526c71
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2024-01-29 07:08:14 +0100

    Agregado de CORS

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,8 +29,9 @@ LOCAL_APSS = [
 
 THIRD_APPS = [
     'rest_framework',
+    'corsheaders',
     'rest_framework.authtoken',
-    'drf_yasg'
+    'drf_yasg',
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APSS + THIRD_APPS 
@@ -41,6 +42,7 @@ TOKEN_EXPIRED_AFTER_SECONDS = 900
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -101,7 +103,13 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'users.User'
 
+# Configuracion de CORS , admite peticiones de otras dirreciones.
+
+CORS_ALLOWED_ORIGINS = [
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
